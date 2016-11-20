5428f28202b85a0c1a1d42a37158953876393d9f
Martin Kloeppner <martin.kloeppner@innogames.com>
2016-11-20 12:09:57 +0100

    Initial Commit

---
+++ MKSimpleDI/MKSimpleDI.h
@@ -0,0 +1,19 @@
+//
+//  MKSimpleDI.h
+//  MKSimpleDI
+//
+//  Created by Martin Kloeppner on 20/11/16.
+//  Copyright © 2016 Martin Kloeppner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MKSimpleDI.
+FOUNDATION_EXPORT double MKSimpleDIVersionNumber;
+
+//! Project version string for MKSimpleDI.
+FOUNDATION_EXPORT const unsigned char MKSimpleDIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKSimpleDI/PublicHeader.h>
+
+
---
+++ MKSimpleDITests/MKSimpleDITests.m
@@ -0,0 +1,39 @@
+//
+//  MKSimpleDITests.m
+//  MKSimpleDITests
+//
+//  Created by Martin Kloeppner on 20/11/16.
+//  Copyright © 2016 Martin Kloeppner. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MKSimpleDITests : XCTestCase
+
+@end
+
+@implementation MKSimpleDITests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
