eb5e31000118a41ff8ebfda5a0a2066abedc9dec
Martin Kloeppner <martin.kloeppner@innogames.com>
2016-11-20 12:16:44 +0100

    Intial

---
+++ MKSimpleDI/MKDIContainer.m
@@ -0,0 +1,13 @@
+//
+//  MKDIContainer.m
+//  MKSimpleDI
+//
+//  Created by Martin Kloeppner on 20/11/16.
+//  Copyright © 2016 Martin Kloeppner. All rights reserved.
+//
+
+#import "MKDIContainer.h"
+
+@implementation MKDIContainer
+
+@end
